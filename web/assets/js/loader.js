e178ccdb087282d01f8eaffe5d44147596afab48
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2023-12-10 23:24:43 -0300

    Add error handling and loading spinner to cargaVista function

--- web/assets/js/loader.js
+++ web/assets/js/loader.js
@@ -1,11 +1,20 @@
 function cargaVista ( url, id ) {
+  // muestra loader mientras carga la vista
+  $( id ).html( `
+    <div class="d-flex justify-content-center pt-4">
+      <div class="spinner-border text-light mt-4" role="status">
+        <span class="visually-hidden">Cargando...</span>
+      </div>
+    </div>
+  `);
+
   try {
     $( id ).load( url, function ( response, status, xhr ) {
       if ( status == "error" ) {
-        var msg = "Ha ocurrido un error: ";
+        var msg = `Ha ocurrido un error: ${xhr.status} ${xhr.statusText}`;
         $( id ).html(
           `<div class="alert alert-danger" role="alert">
-              ${msg} ${xhr.status} ${xhr.statusText}
+            ${msg}
           </div>`
         );
       }
@@ -13,5 +22,10 @@ function cargaVista ( url, id ) {
   }
   catch ( err ) {
     console.log( err );
+    $( id ).html( `
+      <div class="alert alert-danger" role="alert">
+        ${err}
+      </div>
+    `);
   }
 }
