dd40d602c9f8db7157490db3646ebb8f7a442d17
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2023-12-14 01:12:21 -0300

    Refactor loader.js: Add URL search parameter and console.log() statement

--- web/assets/js/loader.js
+++ web/assets/js/loader.js
@@ -1,15 +1,21 @@
 function cargaVista ( url, id ) {
+  // console.log( { search: window.location.search } );
   // muestra loader mientras carga la vista
   $( id ).html( `
-    <div class="d-flex justify-content-center pt-4">
-      <div class="spinner-border text-light mt-4" role="status">
-        <span class="visually-hidden">Cargando...</span>
-      </div>
-    </div>
+  <div class="d-flex justify-content-center pt-4">
+  <div class="spinner-border text-light mt-4" role="status">
+  <span class="visually-hidden">Cargando...</span>
+  </div>
+  </div>
   `);
 
   try {
-    $( id ).load( url, function ( response, status, xhr ) {
+    const newURL = new URL( url, window.location.origin );
+    newURL.search = window.location.search;
+    url = newURL.toString();
+
+    console.log( { url } );
+    $( id ).load( url, function ( _, status, xhr ) {
       if ( status == "error" ) {
         var msg = `Ha ocurrido un error: ${xhr.status} ${xhr.statusText}`;
         $( id ).html(
