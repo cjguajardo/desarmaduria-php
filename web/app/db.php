6b92dc73074c0a200670e4d09570c05171d4f072
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2023-12-14 01:12:08 -0300

    Add function to close database connection
    This commit adds a new function `cerrarConexion()` to the `db.php` file that allows for closing the database connection. This function can be used to properly close the connection after it is no longer needed.

--- web/app/db.php
+++ web/app/db.php
@@ -9,7 +9,11 @@ $conn = new mysqli($servername, $username, $password, $database);
 
 // Verificar la conexión
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+  die("Error de conexión: " . $conn->connect_error);
 }
 
-
+function cerrarConexion()
+{
+  global $conn;
+  $conn->close();
+}
